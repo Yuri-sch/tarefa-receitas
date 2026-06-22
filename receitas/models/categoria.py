c7146d00491b5e6796eaf01d1d05c86b819abb50
Yuri-sch <yurischaffer028@gmail.com>
2026-06-22 18:58:38 -0300

    teste

---
+++ receitas/models/categoria.py
@@ -0,0 +1,12 @@
+import math
+from django.db import models
+
+class Categoria(models.Model):
+    descricao = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = 'categoria'
+        app_label = 'receitas'
+
+    def __str__(self):
+        return self.descricao
