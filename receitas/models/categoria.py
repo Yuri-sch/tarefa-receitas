d3a8b2089f43983513ede62b1fb1898afff9c754
Yuri-sch <yurischaffer028@gmail.com>
2026-06-23 17:24:42 -0300

    teste

--- receitas/models/categoria.py
+++ receitas/models/categoria.py
@@ -1,13 +1,11 @@
-#import math
-
-#from django.db import models
-
+# from django.db import models
+# 
 # class Categoria(models.Model):
-    # descricao = models.CharField(max_length=100)
-
-    # class Meta:
-        # db_table = 'categoria'
-        # app_label = 'receitas'
-
-    #def __str__(self):
-        #return self.descricao
+#     descricao = models.CharField(max_length=100)
+# 
+#     class Meta:
+#         db_table = 'categoria'
+#         app_label = 'receitas'
+# 
+#     def __str__(self):
+#         return self.descricao
